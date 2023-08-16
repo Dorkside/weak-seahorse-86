69110e24be226742768788e79a9e1af70738109b
Dorkside <james.martin@axa.com>
2023-08-16 16:21:24 +0200

    fix: await body value

--- main.ts
+++ main.ts
@@ -12,16 +12,9 @@ router
     if (!context.request.hasBody) {
       context.throw(415);
     }
-    const body = await context.request.body();
-    if (body.type !== "json") {
-      context.throw(415);
-    }
+    const requestBody = await context.request.body().value;
 
-    console.log("body", body.value);
-
-    const { situation } = JSON.parse(body.value);
-
-    console.log("situation", situation);
+    console.log("situation", requestBody.situation);
 
     let response;
 
@@ -45,7 +38,7 @@ router
                 "state": [
                   { "id": "now", "value": 1692184507594, "order": -1 },
                   { "id": "today", "value": 1692144000000, "order": -1 },
-                  ...Object.entries(situation).map(([key, value]) => ({ "id": key, "value": value, "order": 0 }))
+                  ...Object.entries(requestBody.situation).map(([key, value]) => ({ "id": key, "value": value, "order": 0 }))
                 ],
                 "line_ref": "root"
               }
