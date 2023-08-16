61a451a2e79fb4c247e48dd5bad2883f9a5e17c8
Dorkside <james.martin@axa.com>
2023-08-16 15:09:32 +0200

    fix: assert type json

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
-import openapi from "./openapi.json";
-import apiPlugin from "./api-plugin.json";
+import openapi from "./openapi.json" assert { type: "json" };
+import apiPlugin from "./api-plugin.json" assert { type: "json" };
 
 const router = new Router();
 router
