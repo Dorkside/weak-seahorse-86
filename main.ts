f0b48c288a2cef3fbea7580cc17c92baa755cec1
Dorkside <james.martin@axa.com>
2023-08-16 16:15:08 +0200

    feat: add cc call

--- main.ts
+++ main.ts
@@ -8,8 +8,56 @@ router
   .get("/", (context) => {
     context.response.body = "Welcome to the Ask API!";
   })
-  .post("/ask", (context) => {
-    context.response.body = "Situation is missing information. Does the user have EXTENDED_WARRANTY?";
+  .post("/ask", async (context) => {
+    if (!context.request.hasBody) {
+      context.throw(415);
+    }
+    const body = context.request.body();
+    if (body.type !== "json") {
+      context.throw(415);
+    }
+    const { situation } = body.value;
+
+    let response;
+
+    try {
+      response = await fetch("https://productmodeler.axa.com/api/v4/tenants/partners-motor-and-home/computation", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "x-api-key": "sJikTZ90JqzqpvIsarlIc7A3EtuyohHFrTTKcoJCBO4aGTvGwHplmo2k40bw",
+        },
+        body: JSON.stringify({
+          "productId": "141434",
+          "release": "initial",
+          "policy": {
+            "endorsements": []
+          },
+          "match": [],
+          "claim": {
+            "lines": [
+              {
+                "state": [
+                  { "id": "now", "value": 1692184507594, "order": -1 },
+                  { "id": "today", "value": 1692144000000, "order": -1 },
+                  ...Object.entries(situation).map(([key, value]) => ({ "id": key, "value": value, "order": 0 }))
+                ],
+                "line_ref": "root"
+              }
+            ]
+          },
+          "ignoreTriggers": false,
+          "onlyNext": false
+        })
+      });
+    } catch (error) {
+      context.throw(500, error);
+    }
+
+    if (response.status === 200) {
+      const { result } = await response.json();
+      context.response.body = result.nextQuestion;
+    }
   })
   .get("/openapi.json", (context) => {
     context.response.body = openapi;
